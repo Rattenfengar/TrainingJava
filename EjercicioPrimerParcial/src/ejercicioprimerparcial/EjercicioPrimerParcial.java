1925da0f248b0c8f57e77d8248ca8fb45d2506cc
Rattenfengar <37422018+Rattenfengar@users.noreply.github.com>
2019-02-15 23:15:22 -0500

    Comments Added

--- EjercicioPrimerParcial/src/ejercicioprimerparcial/EjercicioPrimerParcial.java
+++ EjercicioPrimerParcial/src/ejercicioprimerparcial/EjercicioPrimerParcial.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 /**
  *
  * @author Alejandro Ordoñez
+ * Para ver la documentacion del proyecto ir a: https://github.com/Rattenfengar/TrainingJava
  */
 public class EjercicioPrimerParcial {
 
@@ -19,7 +20,9 @@ public class EjercicioPrimerParcial {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Creación del objeto Scanner
         Scanner input = new Scanner(System.in);
+        //Definición de las variables
         String Nombre = "";
         int opcion = 0;
         double resultado = 0;
@@ -32,43 +35,68 @@ public class EjercicioPrimerParcial {
         System.out.println("Digite su nombre: ");
         //Toma el valor de Scanner en la siguiente línea.
         Nombre = input.nextLine();
+        //Imprime el nombre junto con la bienvenida.
         System.out.println("Bienvenido: " + Nombre);
+        //Muestra un menú
         System.out.println("1) Calcular cuadrados");
         System.out.println("2) Calcular Raiz cuadrada");
         System.out.println("3) Calcular promedio de 3 notas");
         System.out.println("Ingrese la opcion deseada");
+        //Verifica que no se ingrese otro dato diferente de Int
         if (input.hasNextInt()) {
+            //Almacena el dato en la variable opción
             opcion = input.nextInt();
-            if (opcion < 1 && opcion > 4) {
-                System.out.println("Error, opcion fuera de rango");
-                System.exit(0);
-            }
         } else {
+            //Da un mensaje de error y termina el programa
             System.out.println("Error, Ingresó dato incorrecto");
             System.exit(0);
         }
+        //Evaluar la opción ingresada
         if (opcion == 1) {
             System.out.println("Ha escogido la función calcular cuadrados");
             System.out.println("Ingrese el valor");
-            r1 = input.nextDouble();
-            resultado = Math.pow(r1, 2);
+            if(input.hasNextDouble()){ //Verifica que el usuario ingrese un numero
+                r1 = input.nextDouble();
+            }else{
+                System.out.println("Error, dato erroneo ingresado");
+                System.exit(0);
+            }
+            //Calcula y almacena el resultado en la variable resultado
+            resultado = Math.pow(r1, 2); //pow - para potencias
             System.out.format("El resultado es: %.2f %n", resultado);
         } else if (opcion == 2) {
             System.out.println("Ha escogido la función raiz cuadrada");
             System.out.println("Ingrese el valor");
-            r1 = input.nextDouble();
-            resultado = Math.sqrt(r1);
+            if(input.hasNextDouble()){
+                r1 = input.nextDouble();
+            }else{
+                System.out.println("Error, dato erroneo ingresado");
+            }
+            resultado = Math.sqrt(r1); // sqrt para raíz cuadrada
             System.out.format("El resultado es: %.2f %n", resultado);
         } else if (opcion == 3) {
             System.out.println("Sacar promedio");
             System.out.println("Ingrese nota 1");
-            r1 = input.nextDouble();
+            if(input.hasNextDouble()){
+                r1 = input.nextDouble();
+            }else{
+                System.out.println("Error, dato erroneo ingresado");
+            }
             System.out.println("Ingrese nota 2");
-            r2 = input.nextDouble();
+            if(input.hasNextDouble()){
+                r2 = input.nextDouble();
+            }else{
+                System.out.println("Error, dato erroneo ingresado");
+            }
             System.out.println("Ingrese nota 3");
-            r3 = input.nextDouble();
+            if(input.hasNextDouble()){
+                r3 = input.nextDouble();
+            }else{
+                System.out.println("Error, dato erroneo ingresado");
+            }
             resultado = (r1 + r2 + r3) / 3;
             System.out.format("Su promedio de notas es: %.2f %n", resultado);
+            //Clasificación del desempeño
             if (resultado >= 0 && resultado < 3) {
                 System.out.println("Su desempeño es bajo!");
             } else if (resultado >= 3 && resultado < 4) {
@@ -77,9 +105,10 @@ public class EjercicioPrimerParcial {
                 System.out.println("Su desempeño es Alto");
             }
         } else {
-            System.out.println("Opcion no valida");
+            //Si se digita una opción fuera del rango 1-3 da un error
+            System.out.println("Error, Opcion fuera de rango");
+            System.exit(0);
         }
-
     }
 
 }
