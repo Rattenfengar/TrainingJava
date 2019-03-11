e6b8ed0acb5332fe6556a781d2c0a047439a6576
Rattenfengar <37422018+Rattenfengar@users.noreply.github.com>
2019-03-11 17:22:02 -0500

    Update Taller4.java

--- Taller4/src/taller4/Taller4.java
+++ Taller4/src/taller4/Taller4.java
@@ -31,6 +31,8 @@ public class Taller4 {
     public static void main(String[] args) {
         // TODO code application logic here
         bienvenida();
+        navegacion(menu());
+        
     }
     /**
      * Método que da la bienvenida.
@@ -46,12 +48,71 @@ public class Taller4 {
         System.out.format ("|*******************************BIENVENIDO: %10s ******************************|", nombre);
         System.out.println("|******************************************************************************|\n");
     }
+    /**
+     * Este metodo muestra el menu
+     * @return es el valor de la opcion digito el usuario.
+     */
     public static byte menu(){
         System.out.println("|******************************************************************************|");
         System.out.println("| 1) Tabulación de funciones...................................................|");
         System.out.println("| 2) Calculadora...............................................................|");
         System.out.println("| 3) Comprobar texto...........................................................|");
-        System.out.println("| 4) ");
+        System.out.println("| 4) Unir texto................................................................|");
+        System.out.println("| 5) Salir.....................................................................|");
+        return obtenerByte();
+    }
+    /**
+     * Este metodo define la navegacion del programa
+     * @param opcion opcion que recibe del metodo menu
+     */
+    public static void navegacion(byte opcion){
+     switch(opcion){
+         case 1:
+             break;
+         case 2:
+             break;
+         case 3:
+             break;
+         case 4:
+             break;
+         case 5:
+             System.out.println("Gracias por usar el programa");
+             System.exit(0);
+             break;
+         default:
+             System.out.println("Opción Incorrecta......");
+             navegacion(menu());
+             break;
+     }   
+    }
+    /**
+     * Este metodo tabula una funcion polinomial
+     * @param start inicio de la tabulacion
+     * @param end fin de la tabulacion.
+     */
+    public static void tabFunction(double start, double end){
+        byte order = 0;
+        double coeficiente=0;
+        double resultado;
+        System.out.println("Las funcione que ingrese deberan ser polinomiales de la forma: x^2+3x+5");
+        System.out.println("Ingrese el orden de la ecuacion: ");
+        order=obtenerByte();
+        
+        for (int i = 0; i < order; i++) {
+            System.out.println("Coeficiente de grado: "+i);
+            
+        }
+    }
+    public static byte obtenerByte(){
+        byte x =0;
+        if(input.hasNextByte()){
+            x = input.nextByte();
+            return x;
+        }else{
+            System.out.println("Error dato incorrecto");
+            System.exit(0);
+        }
+        return Byte.MAX_VALUE;
     }
     
 }
