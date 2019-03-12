2c099796193598efe7ffed6cbdeb45e92135b6ae
Rattenfengar <37422018+Rattenfengar@users.noreply.github.com>
2019-03-12 14:18:29 -0500

    new Methods

--- Taller4/src/taller4/Taller4.java
+++ Taller4/src/taller4/Taller4.java
@@ -68,6 +68,7 @@ public class Taller4 {
     public static void navegacion(byte opcion){
      switch(opcion){
          case 1:
+             tabFunction();
              break;
          case 2:
              break;
@@ -90,21 +91,28 @@ public class Taller4 {
      * @param start inicio de la tabulacion
      * @param end fin de la tabulacion.
      */
-    public static void tabFunction(double start, double end){
+    public static void tabFunction(){
+        int start=0, end=0;
         byte x = 0;
-        double r1,r2,r3, r4 =0;
+        double r1=0,r2=0,r3=0, r4 =0;
+        System.out.println("Ingrese el valor inicial desde donde dessea tabular");
+        start=obtenerInt();
+        System.out.println("Ingrese el valor final hasta donde dessea tabular");
+        end=obtenerInt();
         System.out.println("|******************************************************************************|");
         System.out.println("| 1) Lineal y=ax+b.............................................................|");
         System.out.println("| 2) Cuadrática y=ax^2+bx+c....................................................|");
         System.out.println("| 3) Cúbica y=ax^3+bx^2+cx+d..............................................................|");
         System.out.println("|******************************************************************************|");
         System.out.println("Ingrese la opción deseada...");
+        x=obtenerByte();
         switch(x){
             case 1:
                 System.out.println("Ingrese el coeficiente a");
                 r1=obtenerDouble();
                 System.out.println("Ingrese el coeficiente b");
                 r2=obtenerDouble();
+                printFunction("Lineal",start, end, x, r1, r2, r3,r4);
                 break;
             case 2:
                 System.out.println("Ingrese el coeficiente a");
@@ -113,6 +121,7 @@ public class Taller4 {
                 r2=obtenerDouble();
                 System.out.println("Ingrese el coeficiente c");
                 r3=obtenerDouble();
+                printFunction("Cuadratica",start, end, x, r1, r2, r3,r4);
                 break;
             case 3:
                 System.out.println("Ingrese el coeficiente a");
@@ -123,10 +132,43 @@ public class Taller4 {
                 r3=obtenerDouble();
                 System.out.println("Ingrese el coeficiente d");
                 r4=obtenerDouble();
+                printFunction("Cubica",start, end, x, r1, r2, r3,r4);
                 break;
             default:
                 break;
         }
+    }
+    public static double calcLinear(double a, double b, double x){
+        return a*x+b;
+    }
+    public static double calcCubic(double a, double b, double c,double d, double x){
+        return a*Math.pow(x, 3)+b*Math.pow(x, 2)+c*x+d;
+    }
+    public static double calcQuadratic(double a, double b, double c, double x){
+        return a*Math.pow(x, 2)+b*x+c;
+    }
+    public static void printFunction(String msg, int init, int end, byte z, double a, double b, double c, double d){
+        System.out.println("Tabulacion funcion "+msg);
+        System.out.println("|***************************************");
+        System.out.println("|        x          |           y        |");
+        switch(z){
+            case 1:
+                for (int i = init; i <= end; i++) {
+                    System.out.printf( "|     %4d          |        %5.4f      |\n", i, calcLinear(a,b,i) );
+                }
+                break;
+            case 2:
+                for (int i = init; i <=end; i++) {
+                    System.out.printf( "|     %4d          |        %5.4f      |\n", i, calcQuadratic(a,b,c,i) );
+                }
+                break;
+            case 3:
+                for (int i = init; i <= end; i++) {
+                    System.out.println("|        x          |           y        |");
+                    System.out.printf( "|     %4d         |        %5.4f      | \n", i, calcCubic(a,b,c,d,i) );
+                }
+        }
+        
     }
     public static byte obtenerByte(){
         byte x =0;
@@ -149,6 +191,17 @@ public class Taller4 {
             System.exit(0);
         }
         return Double.NaN;
+    }
+    public static int obtenerInt(){
+         int x =0;
+        if(input.hasNextInt()){
+            x = input.nextInt();
+            return x;
+        }else{
+            System.out.println("Error dato incorrecto");
+            System.exit(0);
+        }
+        return Integer.MAX_VALUE;
     }
     
 }
